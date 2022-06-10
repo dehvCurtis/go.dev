166f22994f20bc601120a31583150450f69373b5
Dehvon Curtis <dcurtis@datto.com>
2022-06-10 08:45:23 -0600

    commit

--- tutorials/Create_a_Go_module/greetings/greetings.go
+++ tutorials/Create_a_Go_module/greetings/greetings.go
@@ -2,14 +2,9 @@ package greetings
 
 import "fmt"
 
-// Hello returns a greeting for the named person
-func Hello() {
-    message := fmt.Sprintf("Hi %v. Welcome!", name)
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
     return message
 }
-
-// Alternative way to write this function
-// func Hello () {
-//     var message string
-//     message = fmt.Sprintf("Hi %v. Welcome!", name)
-// }
