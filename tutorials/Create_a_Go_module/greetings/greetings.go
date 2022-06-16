3ddad8bda26ea66662b97b6393fe3b4eca4bca5c
Dehvon C <dehvonc@gmail.com>
2022-06-16 15:15:52 -0600

    commit

--- tutorials/Create_a_Go_module/greetings/greetings.go
+++ tutorials/Create_a_Go_module/greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+func Hello(name string,error) string {
+	// If no name is given, return an error with a message
+	if name == {
+		return "", errors.New("empty name")
+	}
+	
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
